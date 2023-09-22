6a96ef9ddeb8bd31a23bba0114ba9316ff241fd6
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-09-22 11:10:45 -0300

    initial commit

---
+++ HackaTruco/HackaTruco/HackaTrucoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackaTrucoApp.swift
+//  HackaTruco
+//
+//  Created by Natan de Camargo Rodrigues on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HackaTrucoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
