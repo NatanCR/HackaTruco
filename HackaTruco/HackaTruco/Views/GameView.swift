6523db6e6b38b774e029565462f0e069607d5d8c
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-09-27 16:21:14 -0300

    feat: adicionando cartas ao jogo

--- HackaTruco/HackaTruco/Views/GameView.swift
+++ HackaTruco/HackaTruco/Views/GameView.swift
@@ -5,26 +5,21 @@ struct GameView: View {
     @ObservedObject var controllerAPI: ApiRequest
     @State var player1 = PlayerModel()
     @State var player2 = PlayerModel()
+    @State var table: TableModel? = nil
     @State var shackle: CardModel? = nil
+    
     
     var body: some View{
         VStack(content: {
             ScoreView(scorePlayer: 10, scoreCPU: 12, round: 2)
-            CardComponent(imageCard: [Image(uiImage: UIImage(named: "card")!)], isPlayer: false)
-            HStack(spacing: -80){
-                AsyncImage(
-                    url: URL(string: shackle?.image ?? String()),
-                    content: { image in
-                        image.resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 152, maxHeight: 155)
-                    },
-                    placeholder: {
-                        ProgressView()
-                    })
-                Image(uiImage: UIImage(named: "card")!)
+            CardComponent(imageCard: $player2, isPlayer: false)
+            Spacer()
+            HStack(spacing: -50){
+                ImageCardComponent(url: shackle?.image ?? String())
+                Image(uiImage: UIImage(named: "card") ?? .strokedCheckmark).resizable().frame(maxWidth: 72,maxHeight: 100)
             }
-//            CardComponent(imageCard: [], isPlayer: <#Bool#>)
+            Spacer()
+            CardComponent(imageCard: $player1, isPlayer: true)
             Spacer()
             
             Button {
--- HackaTruco/HackaTruco/Views/HomeView.swift
+++ HackaTruco/HackaTruco/Views/HomeView.swift
@@ -48,8 +48,8 @@ struct HomeView: View {
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
-}
+//struct HomeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView()
+//    }
+//}
