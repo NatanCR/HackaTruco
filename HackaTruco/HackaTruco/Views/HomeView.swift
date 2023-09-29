0af98858ef42c2f0f46f825f730f650518f35604
Gustavo Assis <guu.assis@hotmail.com>
2023-09-29 02:20:44 -0300

    Detalhes tela ininicio e game

--- HackaTruco/HackaTruco/Views/HomeView.swift
+++ HackaTruco/HackaTruco/Views/HomeView.swift
@@ -11,30 +11,74 @@ struct HomeView: View {
     @ObservedObject var vm: GameStatus = GameStatus()
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color("bgHomeColor").ignoresSafeArea()
-                Image("bgHome")
-                    .resizable()
-                    .frame(height: 1000)
+        NavigationStack{
+            
+            GeometryReader{ geo in
                 VStack{
-                    Text("HackaTruco")
-                        .foregroundColor(.white)
-                        .font(.system(size: 45, weight: .medium, design: .monospaced))
-                        .padding()
-                    NavigationLink(destination: GameView(controllerAPI: controllerAPI), label: {Image("btnJogar")})
-                        .padding()
-                    NavigationLink(destination: RegrasView()) {
-                        Image("btnRegras")
-                    } .padding()
-                    NavigationLink(destination: EstatisticasView(), label: {
-                        Image("btnEstatisticas")
-                    })
-                    .padding()
+                    VStack(spacing: 0){
+                        HStack{
+                            Text("HACKA")
+                                .font(.system(size: 75, weight: .black, design: .rounded))
+                                .foregroundColor(.white)
+                                .frame(height: geo.size.height * 0.1, alignment: .trailing)
+                                .padding(.leading)
+                        }
+                        .frame(width: geo.size.width, height: geo.size.height * 0.11, alignment: .bottomLeading)
+                        
+                        HStack{
+                            Text("truco")
+                                .font(.system(size: 70, weight: .regular, design: .rounded))
+                                .foregroundColor(.white)
+                                .frame(width: geo.size.width * 0.85, alignment: .topTrailing)
+                                .padding(.trailing)
+                        }
+                        .frame(width: geo.size.width, height: geo.size.height * 0.1)
+                        .background(Color("secundaryColor"))
+                    }
+                    .frame(height: geo.size.height * 0.4, alignment: .bottom)
+                    
+                    
+                    VStack(spacing: 30){
+                        NavigationLink(destination: GameView(controllerAPI: controllerAPI)) {
+                            Text("Jogar")
+                                .textCase(.uppercase)
+                                .font(.system(.headline).weight(.bold))
+                                .frame(width: geo.size.width * 0.4, height: 50)
+                                .background(Color("secundaryColor"))
+                                .cornerRadius(10)
+                        }
+                        
+                        NavigationLink(destination: RegrasView()) {
+                            Text("Regras")
+                                .textCase(.uppercase)
+                                .font(.system(.headline).weight(.bold))
+                                .frame(width: geo.size.width * 0.4, height: 50)
+                                .background(Color("secundaryColor"))
+                                .cornerRadius(10)
+                        }
+                        
+                        NavigationLink(destination: EstatisticasView()) {
+                            Text("Estatísticas")
+                                .textCase(.uppercase)
+                                .font(.system(.headline).weight(.bold))
+                                .frame(width: geo.size.width * 0.4, height: 50)
+                                .background(Color("secundaryColor"))
+                                .cornerRadius(10)
+                        }
+                        
+                    }.frame(width: geo.size.width, height: geo.size.height * 0.4, alignment: .bottom)
+                    
                 }
+                .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                .background(
+                    ZStack{
+                        Color("bgHomeColor").ignoresSafeArea()
+                        Image("bgHome").resizable().aspectRatio(contentMode: .fill)
+                    }
+                )
+                
                 
             }
-            
         }
         
         .onAppear {
@@ -47,5 +91,12 @@ struct HomeView: View {
             }
         }
         .accentColor(.white)
+    }
+}
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
     }
 }
