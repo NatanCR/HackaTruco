2cbb933399dec99145420b3f08c34df8c59b89d4
Luana Moraes <90534155+luumoraes@users.noreply.github.com>
2023-09-28 22:15:19 -0300

    animacao gameview

--- HackaTruco/HackaTruco/Views/GameView.swift
+++ HackaTruco/HackaTruco/Views/GameView.swift
@@ -4,17 +4,40 @@ struct GameView: View {
     @ObservedObject var controllerAPI: ApiRequest
     @State var player1 = PlayerModel()
     @State var player2 = PlayerModel()
+    @State var showMsg: Bool = false
+
     
     var body: some View{
         VStack(content: {
             ScoreView(scorePlayer: 10, scoreCPU: 12, round: 2)
             Spacer()
-            
+            if showMsg {
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundColor(Color("red")).bold()
+                    .frame(maxWidth: 200, maxHeight: 50)
+                    .overlay(
+                        Text("Partida duplicada")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white).bold()
+                    )
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2, execute: {
+                            self.showMsg.toggle()
+                        })
+                    }
+                Spacer()
+            }
             Button {
                 dump(player1.handCards)
                 dump(player2.handCards)
             } label: {
                 Text("Set Cards")
+            }
+            Button {
+                    self.showMsg.toggle()
+                
+            } label: {
+                Text("Truco")
             }
             
         })
